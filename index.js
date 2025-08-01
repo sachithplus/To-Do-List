ffdd42043e5a43b52e6a90b6659545eda4af7029
Sachith Waidyasekara <sachith.plus@gmail.com>
2025-07-31 23:53:25 -0700

    Add to-do list JS: create, delete, and mark tasks complete
    Implement full to-do list functionality: add tasks, delete tasks, and toggle completion

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 document.querySelector('#push').onclick = function (){
-if(document.querySelector('#newtask input').value.length === 0)
+
+let taskInput = document.querySelector("#newtask input");
+
+
+if(taskInput.value.length === 0)
 {
-alert("Please Enter a Task")
+alert("Please Enter a Task");
+
+} else{
+let taskList = document.querySelector("#task");
+let newTask = document.createElement('div');
+newTask.classList.add("task");
+
+newTask.innerHTML = `
+<span>${taskInput.value}</span>
+<button class="delete"> <i class="fa fa-trash" aria-hidden="true"></i></button>
+
+`;
+taskList.appendChild(newTask);
+taskInput.value = "";
+
+newTask.querySelector("span").onclick = function (){
+this.classList.toggle("completed");
+
+};
+
+
+newTask.querySelector(".delete").onclick = function (){
+newTask.remove();
+
+};
 
 }
-}
+};
